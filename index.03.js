43cf57d5f2ab73558e2ee43296342bd95f575afd
SidathDabare <sidath2007@gmail.com>
2022-04-05 16:48:26 +0200

    JavaScript-HomeWork

---
+++ index.03.js
@@ -0,0 +1,63 @@
+// Additional assignments for Day 3
+
+/* EXTRA 1
+ Use a ternary operator to assign to a variable called gender the string values "male" or "female".
+ The choice should be made based on the value of another variable called isMale.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 2
+ Write a piece of code for checking if, given two integers, the value of one of them is 8 or if their addition or subtraction is equal to 8.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 3
+ Create a variable and assign to it the concatenation of two strings.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 4
+ Create three variables and assign a numerical value to each one of them. 
+ Using a conditional statement, write a piece of code for sorting their values from highest to lowest.
+ Display the result in the console.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 5
+ Write a piece of code for finding the average of two given integers.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 6
+ Write a piece of code for finding the longest of two given strings.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 7
+ Write a piece of code for checking if a given value is a integer or not.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 8
+ Write a piece of code for calculating a certain percentage of a given number.
+ (Ex.: the 20% of 400 is 80)
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 9
+ Write a piece of code for checking if a given number is even or odd.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* WHEN YOU ARE FINISHED
+ Upload the .js file on Eduflow before 5PM CET. In the next days we'll also learn how to use GIT!
+*/
---
+++ index01.js
@@ -0,0 +1,59 @@
+console.log("something");
+console.log(4); // prints 4 in the console
+console.log(-6); // prints -6 in the console
+console.log(6.82); // prints 6.82 in the consoleconsole.log(2.998e3) // prints 2998 in the console
+
+console.log(4 + 6); // prints 10 in the console
+console.log(5 * 7); // prints 35 in the console
+console.log(14 - 21); // prints -7 in the console
+console.log(5 / 7); // prints 0.7142857142857143 in the console
+
+console.log(5 % 2); // prints the remainder of 5:2 --> 1
+
+console.log(Infinity);
+console.log(-Infinity);
+console.log(NaN);
+
+console.log("Hello Strivers I am a string!");
+console.log("Hello Strivers I am a string too!");
+console.log(`Hello Strivers I have backticks but I am a string too!`);
+
+console.log("Hello " + "Strivers!");
+
+console.log("Hello Strivers!".toUpperCase());
+console.log("Hello Strivers!".toLowerCase());
+
+console.log(parseInt("100"));
+let number = 100; // let is a keyword which declares a variable, a concept explained down below
+console.log(number.toString());
+console.log(number.toString(2)); // you can even convert a number into his binary form, or hexadecimal form or what you prefer
+
+console.log(null);
+console.log(undefined);
+
+console.log(true);
+console.log(false);
+
+console.log(2 === 2); // Is 2 equal to 2? true
+console.log(3 > 2); // Is 3 greater than 2? true
+console.log(3 < 2); // Is 3 lower than 2? false
+
+console.log(true && true); // true AND true --> true
+console.log(true && false); // true AND false --> false
+console.log(false && true); // false AND true --> false
+console.log(false && false); // false AND false --> false
+
+console.log(true || true); // true OR true --> true
+console.log(true || false); // true OR false --> true
+console.log(false || true); // false OR true --> true
+console.log(false || false); // false OR false --> false
+
+console.log(!true); // NOT true --> false
+console.log(!false); // NOT false --> true
+
+let ten = 10;
+console.log(ten * ten); // --> 100
+let myString = "Hello!";
+console.log(myString); // --> "Hello"
+let isTrue = true;
+console.log(isTrue); // --> true
